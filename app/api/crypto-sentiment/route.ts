b6e3a5ae308b7cc8c214377c3aa26eba0e740db0
Sam Cleland <edward.cleland@mail.mcgill.ca>
2025-11-19 23:51:02 -0500

    added new files

--- app/api/crypto-sentiment/route.ts
+++ app/api/crypto-sentiment/route.ts
@@ -43,7 +43,7 @@ export async function GET() {
       : "http://localhost:3000";
 
     console.log(`${baseUrl}`)
-    const res = await fetch(`${baseUrl}/api/crypto-sentiment-pyth`, {
+    const res = await fetch(`${baseUrl}/api/crypto-sentiment`, {
       cache: "no-store",
     });
 
